d1b4f04e48c5a237c8a56b0c96dda0e22add2a46
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2022-01-04 00:49:04 -0300

    Add files via upload

---
+++ login/setting.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    var eles = document.getElementsByClassName('spec-a1');
+    console.log(eles);
+    console.log(eles.length);
+    console.log(eles[0]);
+})
+
+function setting() {
+    var nameser = document.querySelector('input.spec-a1')
+    console.log(nameser)
+}
